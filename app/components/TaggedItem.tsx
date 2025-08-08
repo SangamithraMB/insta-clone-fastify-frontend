ad9f78de897d687705d74c38c22342111ef9f2a7
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-08-08 14:55:47 +0200

    url updation in taggedgrid

--- app/components/TaggedItem.tsx
+++ app/components/TaggedItem.tsx
@@ -1,13 +1,14 @@
 import type { Tag } from "~/schemas/tagged.schema";
 
 export function TaggedItem({ tag }: { tag: Tag }) {
+  const backendDomain = "https://insta-clone-fastify-backend.onrender.com";
   return (
     <div className="w-full max-w-md mx-auto rounded-2xl overflow-hidden border border-gray-200 bg-white shadow transition hover:shadow-md mb-6">
       <div className="bg-gray-100 px-4 py-2 border-b border-gray-200">
         <p className="text-sm font-medium text-gray-600">📌 Tagged on Personal Page</p>
       </div>
       <img
-        src={tag.img_url}
+        src={`${backendDomain}${tag.img_url}`}
         alt={tag.caption || "Instagram post"}
         className="w-full aspect-square object-cover"
       />

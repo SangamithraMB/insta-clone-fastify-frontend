74c3b57f4b5212fcd88dcf731733c1abe6157005
SangamithraMB <159931656+SangamithraMB@users.noreply.github.com>
2025-08-08 14:15:09 +0200

    vite config updation

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
-const baseURL = import.meta.env.VITE_BACKEND_DEPLOYED || "http://localhost:3000";
+const baseURL = "https://insta-clone-fastify-backend.onrender.com/";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), react()],
